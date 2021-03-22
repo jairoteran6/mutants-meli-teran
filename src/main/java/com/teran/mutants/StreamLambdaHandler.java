94d216dfcdd4b47a246286b12dc99cecfdfa7e8f
Jairo Andres Teran <jairoteran6@gmail.com>
2021-03-21 23:00:49 -0500

    Configuracion deploy en aws lambda, pero genera time out, seguramente por el asincronismo

---
+++ src/main/java/com/teran/mutants/StreamLambdaHandler.java
@@ -0,0 +1,38 @@
+package com.teran.mutants;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamLambdaHandler implements RequestStreamHandler {
+
+    private static SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            //handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(MutantsApplication.class);
+            handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(MutantsApplication.class);
+        } catch (ContainerInitializationException e) {
+            // if we fail here. We re-throw the exception to force another cold start
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+
+        // just in case it wasn't closed by the mapper
+        outputStream.close();
+    }
+}
