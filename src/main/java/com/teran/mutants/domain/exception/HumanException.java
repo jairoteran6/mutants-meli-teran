221a260a0409f9fa5476ebcbc6e715144a0b69e0
Jairo Andres Teran <jairoteran6@gmail.com>
2021-04-28 22:36:49 -0500

    commit 1

--- src/main/java/com/teran/mutants/domain/exception/HumanException.java
+++ src/main/java/com/teran/mutants/domain/exception/HumanException.java
@@ -2,10 +2,8 @@ package com.teran.mutants.domain.exception;
 
 public class HumanException extends RuntimeException {
 
-
     public HumanException(String message) {
         super(message);
-
     }
 
     public HumanException(String message, Throwable causa) {
